985ec6d1c1decc5832832d4c97e1684a3f770cf9
James Morrissey <jammim19@gmail.com>
2026-06-09 21:52:55 +0100

    Enabled the use of scss variables

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [],
   server: {
     port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/scss/_variables.scss" as *;`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   base: process.env.VITE_BASE || "/three-column-preview-card-component",
 });
