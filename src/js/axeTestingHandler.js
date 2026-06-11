d782dbbae17db4f5324b6329ab8e3e10851b3168
James Morrissey <jammim19@gmail.com>
2026-06-11 15:16:10 +0100

    Refactored the axe-core script, so that it's only used in the dev env
    I enabled this to cut down the built bundle size

--- src/js/axeTestingHandler.js
+++ src/js/axeTestingHandler.js
@@ -1,17 +1,21 @@
-import axe from "axe-core";
+if (import.meta.env.DEV) {
+  import("axe-core").then((module) => {
+    const axe = module.default;
 
-window.addEventListener("load", function () {
-  console.log("All assets are loaded");
+    window.addEventListener("load", function () {
+      console.log("All assets are loaded");
 
-  axe
-    .run()
-    .then((results) => {
-      if (results.violations.length) {
-        console.error(results.violations);
-        throw new Error("Accessibility issues found");
-      }
-    })
-    .catch((err) => {
-      console.error("ERROR AXE-01 : ", err.message);
+      axe
+        .run()
+        .then((results) => {
+          if (results.violations.length) {
+            console.error(results.violations);
+            throw new Error("Accessibility issues found");
+          }
+        })
+        .catch((err) => {
+          console.error("ERROR AXE-01 : ", err.message);
+        });
     });
-});
+  });
+}
