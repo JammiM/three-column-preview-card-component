eab10ec717aaf7b8ad607fde48b4509bad8e1b79
James Morrissey <jammim19@gmail.com>
2026-06-11 14:30:47 +0100

    Added 'rollup-plugin-visualizer'

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [
+    visualizer({
+      open: true, // Automatically opens the report in your browser after building
+      filename: "stats.html", // Name of the file to be generated
+      gzipSize: true, // Shows what the size will be after server compression
+      brotliSize: true, // Shows optimized size for modern browsers
+    }),
+  ],
   server: {
     port: 3000,
   },
