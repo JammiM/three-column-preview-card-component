2a51369c0a719428bfaeab9d054c88f1e9db721b
James Morrissey <jammim19@gmail.com>
2026-06-11 20:30:53 +0100

    Added the "stylelint-config-standard-scss" npm package

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ["stylelint-config-standard"],
-  rules: {
-    // You can add overrides here if you disagree with standard formatting
-  },
+  extends: ["stylelint-config-standard-scss"],
 };
