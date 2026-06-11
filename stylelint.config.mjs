2b0e878f56b3d0401f6ef44fce5f327089516b34
James Morrissey <jammim19@gmail.com>
2026-06-11 20:05:48 +0100

    Added stylelint for scss linting

---
+++ stylelint.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    // You can add overrides here if you disagree with standard formatting
+  },
+};
