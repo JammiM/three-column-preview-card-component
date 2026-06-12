97f203e640f6d606055da7081af653e4c2ce1fc9
James Morrissey <jammim19@gmail.com>
2026-06-12 14:38:18 +0100

    Updated the linting config to match the linting config of fem

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ["stylelint-config-standard-scss"],
+  rules: {
+    "media-query-no-invalid": null,
+
+    "block-no-empty": true,
+    "color-no-invalid-hex": true,
+    "declaration-block-no-duplicate-properties": true,
+    "declaration-block-no-shorthand-property-overrides": true,
+    "no-duplicate-selectors": true,
+    "unit-disallowed-list": [
+      ["px"],
+      {
+        ignoreProperties: {
+          px: ["border", "box-shadow", "/^--/"],
+        },
+      },
+    ],
+
+    "property-no-unknown": [
+      true,
+      {
+        // Keeps the linter safe if you use modern CSS modules or features
+        ignoreProperties: ["composes"],
+      },
+    ],
+  },
 };
