0a21acbceec5bc4a2f503791d2e27e6c825441fb
James Morrissey <jammim19@gmail.com>
2026-06-17 16:57:30 +0100

    Refactored the card-generation code

--- src/js/card-generator.js
+++ src/js/card-generator.js
@@ -1,27 +1,32 @@
 import cardData from "./data.json";
 
 export function addCardsToPage(element) {
-  let htmlTemplate = "";
+  if (!element) return;
 
-  cardData.map((card) => {
-    const { heading, description } = card;
+  const fragment = document.createDocumentFragment();
 
+  cardData.forEach(({ heading, description }) => {
     const iconUrl = new URL(
       `../assets/icon-${heading.toLowerCase()}.svg`,
       import.meta.url
     ).href;
 
-    const singleCardTemplate = `
-    <article class="card" data-card-colour=${heading} > 
-        <picture>
-            <img src=${iconUrl} alt="${heading} Logo"/>
-        </picture>
-        <h1 class="card-heading">${heading.toLocaleUpperCase()}</h1>
-        <p class="card-description">${description}</p>
-        <button data-colour=${heading} class="card-learn-btn" type="button">Learn More</button>
-    </article>`;
-    htmlTemplate += singleCardTemplate;
+    const articleElem = document.createElement("article");
+
+    articleElem.className = "card";
+    articleElem.dataset.cardColour = heading;
+
+    articleElem.innerHTML = `
+      <picture>
+        <img src="${iconUrl}" alt="${heading} logo" loading="lazy" />
+      </picture>
+      <h1 class="card-heading">${heading.toUpperCase()}</h1>
+      <p class="card-description">${description}</p>
+      <button data-colour="${heading}" class="card-learn-btn" type="button">Learn More</button>
+    `;
+
+    fragment.append(articleElem);
   });
 
-  element.innerHTML = htmlTemplate;
+  element.replaceChildren(fragment);
 }
